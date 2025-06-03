cc04c169f85943771d4a789779f632da108e7550
unknown <yanzihan@westlake.edu.cn>
2025-06-03 12:36:11 +0800

    update version

--- gpumdkit.sh
+++ gpumdkit.sh
@@ -12,7 +12,7 @@ if [ -z "$GPUMD_path" ] || [ -z "$GPUMDkit_path" ]; then
     exit 1
 fi
 
-VERSION="1.2.7 (dev) (2025-06-03)"
+VERSION="1.2.8 (dev) (2025-06-03)"
 
 #--------------------- function 1 format conversion ----------------------
 # These functions are used to convert the format of the files
