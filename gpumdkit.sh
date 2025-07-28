63283b779b5fa31796bacb500ca142969c154910
unknown <yanzihan@westlake.edu.cn>
2025-07-28 13:03:16 +0800

    improving the robustness of F401

--- gpumdkit.sh
+++ gpumdkit.sh
@@ -12,7 +12,7 @@ if [ -z "$GPUMD_path" ] || [ -z "$GPUMDkit_path" ]; then
     exit 1
 fi
 
-VERSION="1.3.4 (dev) (2025-07-25)"
+VERSION="1.3.5 (dev) (2025-07-28)"
 
 #--------------------- function 1 format conversion ----------------------
 # These functions are used to convert the format of the files
